3f05fcd79217e3968cec422faf210f8dbfeaee4a
kunalchopde <kunalchopde98@gmail.com>
2024-12-11 19:15:05 +0530

    unit test cases updated

---
+++ tests/test_operation.py
@@ -0,0 +1,11 @@
+from src.maths_operation import Add,sub
+
+def Add(a,b):
+    assert(3,2)==5
+    assert(-1,1)==0
+    assert(3,3)==6
+def sub(a,b):
+    assert(3-2)==1
+    assert(1-1)==0
+    assert(5-3)==2
+    assert(3-2)==1
